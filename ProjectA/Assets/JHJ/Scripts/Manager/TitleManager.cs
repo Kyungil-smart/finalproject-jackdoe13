1f303a108e9b1da69c210c44b8c6c72bfb9d93a1
hjchun78-cmd <hjchun78@gmail.com>
2026-07-01 17:59:08 +0900

    [Edit] 타이틀UI와 옵션UI 프리팹분리+제작

--- ProjectA/Assets/JHJ/Scripts/Manager/TitleManager.cs
+++ ProjectA/Assets/JHJ/Scripts/Manager/TitleManager.cs
@@ -4,18 +4,7 @@ using UnityEngine.SceneManagement;
 public class TitleManager : MonoBehaviour
 {
     [Header("씬 이동")]
-    [SerializeField] private string ingameSceneName = "CGS_Ingame";
-
-    [Header("시작 시 비활성화되는 패널들")]
-    [SerializeField] private GameObject[] panelsToHideOnStart;
-
-    private void Start()
-    {
-        foreach (GameObject panel in panelsToHideOnStart)
-        {
-            if (panel != null) panel.SetActive(false);
-        }
-    }
+    [SerializeField] private string ingameSceneName = "JHJ_GameTest1";
 
     public void StartGame()
     {
@@ -25,20 +14,6 @@ public class TitleManager : MonoBehaviour
     public void QuitGame()
     {
         Debug.Log("게임 종료");
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
         Application.Quit();
-#endif
-    }
-
-    public void OpenPanel(GameObject panel)
-    {
-        if (panel != null) panel.SetActive(true);
-    }
-
-    public void ClosePanel(GameObject panel)
-    {
-        if (panel != null) panel.SetActive(false);
     }
 }
